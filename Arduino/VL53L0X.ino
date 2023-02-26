1974e7fa1dba051aee76c72c4e024ee1b8c58118
Pedro H <pedrohenriquegs2001@gmail.com>
2023-02-26 10:27:33 -0300

    Fix: better semantics

--- Arduino/VL53L0X.ino
+++ Arduino/VL53L0X.ino
@@ -9,8 +9,10 @@
 #define VL_PIN01 14
 #define VL_PIN02 27
 
-int tempoAntes;
-int tempo;
+double enterTime;
+double leaveTime;
+double velocityMS;
+double velocityKmH;
 
 Adafruit_VL53L0X VL01 = Adafruit_VL53L0X();
 Adafruit_VL53L0X VL02 = Adafruit_VL53L0X();
@@ -55,38 +57,43 @@ void setup() {
   
 }
 
-void loop(){
-  
+void getVelocity() {
   VL01.rangingTest (& measure1, false);
   VL02.rangingTest (& measure2, false);
   
-  if(measure1.RangeStatus != 4){
-    Serial.print("\nDistancia VL1 (mm): ");
-    Serial.print(measure1.RangeMilliMeter);
-    if(measure1.RangeMilliMeter <= 100) {
-      tempoAntes = millis();
-    } 
-  }
-  else {
-    Serial.print("\n out of range ");
-  }
-  
-  if(measure2.RangeStatus != 4){
-    Serial.print(" | Distancia VL2 (mm): ");
-    Serial.print(measure2.RangeMilliMeter);
-    if(measure2.RangeMilliMeter <= 100) {
-      tempo = millis();
-    } 
-  }else{
-    Serial.print(" | out of range ");
+  if(measure1.RangeStatus != 4 && measure1.RangeMilliMeter <= 100){
+    // When the first sensor measurement is smaller than 10cm, it records the enter time
+    enterTime = micros();
+
+    // While the second sensor measurement is greater than 10cm, it waits
+    while(!(measure2.RangeStatus != 4 && measure2.RangeMilliMeter <= 100)) {
+      VL02.rangingTest (& measure2, false);
+    }
+    // When the second sensor measurement is smaller than 10cm, it records the leave time
+    leaveTime = micros();   
   }
 
-  Serial.println("");
-  Serial.println(tempoAntes);
-  Serial.println(tempo);
-  Serial.print("Tempo de passagem: ");
-  Serial.println(tempo - tempoAntes);
-  Serial.print("Velocidade: ");
-  Serial.println(100.0 / (tempo - tempoAntes));
+  // All velolcity calculations are considering that the distance between the sensors is 100mm
+  velocityMS = (100.0 / (leaveTime - enterTime)) * 1000.0;
+  Serial.print("Velocidade(m/s): ");
+  Serial.println(velocityMS,6);
+
+  velocityKmH = (100.0 / (leaveTime - enterTime)) * 3600.0;
+  Serial.print("Velocidade(km/h): ");
+  Serial.println(velocityKmH,6);
+}
+
+void sendVelocity() {
+  /*
+    !!! TO DO !!!
+    THIS FUNCTION SHOULD SEND THE VELOCITY CALCULATIONS VIA WIFI TO RASPBERRY 
+    PI OR PUBLISH THE DATA DIRECTLY TO ROS.
+  */
+}
+
+void loop(){
+  
+  getVelocity();
+  sendVelocity();
 
 }
